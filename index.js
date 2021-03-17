293a71fbe3e3e605bca1ab5bc40ef4e644bda22d
Dame Sadel Delgado <delgadodame@gmail.com>
2021-03-17 03:59:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ function sayHiTpGrandma(string){
   else if (string === string.toUpperCase)
   return "YES INDEED!"
   
-  else if(string === "I love you, Grandma")
+  else if(string === "I love you, Grandma.")
+  return "I love you, too."
+  
+}
 }
 
